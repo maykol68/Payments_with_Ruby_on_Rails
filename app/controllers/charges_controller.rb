a7139764b8aca42c3f5b5fd62f62478a27104e44
maymine <sm9365277@gmail.com>
2023-07-14 12:02:31 -0500

    created charge controller

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,25 @@
+class ChargesController < ApplicationController
+
+    def index 
+        @charges = Charge.all
+    end
+
+    def new 
+        @charge = Charge.new
+    end
+
+    def create
+        charge = Charge.new(charge_params)
+        if charge.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def charge_params
+        params.require(:charge).permit(:amount)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "charges#index" path: "/"
+
+  resources :charges, only:[:new, :create]
 end
